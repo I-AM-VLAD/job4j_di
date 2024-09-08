82e814adebf26706aab722a525a4de25faa1d71e
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-09-08 17:10:22 +0300

    4. Конфигурация контейнера [#505072]

---
+++ src/main/java/ru/job4j/annotation/Dog.java
@@ -0,0 +1,17 @@
+package ru.job4j.annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog implements Pet<Dog> {
+    private String name = "Dog";
+    public Dog() {}
+
+    public Dog(String name) {
+        this.name = name;
+    }
+    @Override
+    public String print() {
+        return "Dog name = " + name;
+    }
+}
